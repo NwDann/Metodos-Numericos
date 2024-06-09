cf4980bac2622e795d8492f3a24eb58d551a6539
Danny I <118556155+NwDann@users.noreply.github.com>
2024-06-09 18:18:12 -0500

    Proyecto mejorado con biseccion

--- Proyecto I Bimestre/Arquitectura.py
+++ Proyecto I Bimestre/Arquitectura.py
@@ -2,14 +2,24 @@
 #                       PROYECTO ARQUITECTURA - METODOS NUMERICOS
 
 # Paquetes necesarios
-from scipy.optimize import newton
-import math
+from scipy.optimize import bisect
 
-# Funcion que calcula W
-def w(x1, x2, h):
-    root = newton(func = lambda w : (h*w) / (x2**2 - w**2)**0.5 + (h*w) / (x1**2 - w**2)**0.5 - w, x0 = 2.5)
-    return root
-
+# Función que calcula la raiz
+def root(x1, x2, h):
+    # Funcion que calcula W
+    def w(w):
+        return (h*w) / (x2**2 - w**2)**0.5 + (h*w) / (x1**2 - w**2)**0.5 - w
+    # Estimación
+    a = 0.01
+    print(a)
+    b = min(x1, x2) - 0.01
+    print(b)
+    try:
+        root = bisect(w, a = a, b = b)
+        return root
+    except:
+        return None
+    
 # INICIO DEL PROGRAMA
 
 # Control de entrada de enteros usando excepciones
@@ -29,5 +39,9 @@ while True:
     else:
         break
 
-root = w(x1, x2, h)
-print(f"El valor en metros de la distancia o ancho del pasillo para las escaleras y su altura es: {root}")
+root = root(x1, x2, h)
+if root:
+    print(f"El valor en metros de la distancia o ancho del pasillo para las escaleras y su altura es: {round(root, 2)}")
+    
+else:
+    print("No se encontro solución alguna")
